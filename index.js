4a74832ed93ad3ba874e5d769a5d6d05c2dbd210
Basil Homadi <deviltearbasil@gmail.com>
2020-09-18 13:05:31 +0000

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,27 @@ const tutorials = [
 ];
 
 const titleCased = () => {
+  
+  for (let i = 0; i < tutorials.length; i++) {
+
+  let mystring = tutorials[i].toString();
+  let str = mystring.split(" ");
+
+  for (var j = 0; j < str.length; j++) {
+      str[j] = str[j][0].toUpperCase() + str[j].substr(1);
+  }
+
+  let tn = str.join(" ")
+
+  //console.log(tn)
+  tutorials[i] = tn;
+
+}
   return tutorials
 }
+
+
+console.log(titleCased());
+
+let myArrMap = tutorials.map(titleCased);
+console.log(myArrMap);
